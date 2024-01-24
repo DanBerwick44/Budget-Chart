85c4e756effb5aae28f8c381a207be624d70d685
Daniel <Dan.Berwick44+odinproject@gmail.com>
2024-01-23 16:05:28 -0800

    Hotfix upload and parsing

--- javascript.js
+++ javascript.js
@@ -5,7 +5,9 @@ function sortData(input){
 document.getElementById('file').addEventListener('change', function read(Event){
     console.log('File obtained:', Event.target)
     Papa.parse(Event.target,{
-        complete: sortData(results)
+        complete: function(results){
+             console.log(results)
+    }
     })
 
 })
