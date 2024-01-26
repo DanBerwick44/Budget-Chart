e36cee225657ad7eafd41ad526616695fcaf610a
Daniel <Dan.Berwick44+odinproject@gmail.com>
2024-01-25 17:27:02 -0800

    Fix connection from file input to parsing

--- javascript.js
+++ javascript.js
@@ -2,12 +2,20 @@ function sortData(input){
 
 }
 
-document.getElementById('file').addEventListener('change', function read(Event){
-    console.log('File obtained:', Event.target)
-    Papa.parse(Event.target,{
+document.getElementById('file').addEventListener('change', function(Event){
+ 
+    console.log('File obtained:', Event.target.files[0])
+    
+    Papa.parse(Event.target.files[0],{
+        dynamicTyping: false,
+        header: true,
         complete: function(results){
-             console.log(results)
-    }
-    })
-
-})
+            console.log('data parse complete')
+            var parsedData = results.data
+        console.log(parsedData)
+        }
+            
+             
+        })
+}
+)
